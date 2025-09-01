835d51a54e926c040dcedf5da3a698ecc0d915fd
Andrey1166 <aannddrreeyy266@gmail.com>
2025-09-01 07:36:15 +0300

    make build

---
+++ js_task_fix_form_DOM.f75de5e1.js
@@ -0,0 +1,28 @@
+'use strict';
+const forms = [
+    ...document.querySelectorAll('form')
+];
+forms.forEach((form)=>{
+    const inputs = [
+        ...form.querySelectorAll('input')
+    ];
+    inputs.forEach((item)=>{
+        const label = document.createElement('label');
+        const inputName = item.getAttribute('name').replaceAll(/[-_]/g, ' ');
+        let labelName = '';
+        inputName.split('').forEach((char)=>{
+            if (char === char.toUpperCase()) labelName += ' ' + char;
+            else labelName += char;
+        });
+        labelName = labelName.charAt(0).toUpperCase() + labelName.slice(1);
+        if (!item.id) item.id = 'auto-id-' + labelName.toLowerCase().replaceAll(' ', '-');
+        const id = item.getAttribute('id');
+        label.setAttribute('class', 'field-label');
+        label.setAttribute('for', id);
+        item.setAttribute('placeholder', labelName);
+        label.textContent = labelName;
+        item.parentElement.appendChild(label);
+    });
+});
+
+//# sourceMappingURL=js_task_fix_form_DOM.f75de5e1.js.map
